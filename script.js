a8b07b803f5df5e851d68f1378fa0a005a99c43e
jastasovas <jastasovas@gmail.com>
2023-11-29 23:42:03 +0200

    Added units

---
+++ script.js
@@ -0,0 +1,106 @@
+//------------------------------------- Unit 1 --------------------------------------------
+
+// for (let i = 0; i < 10; i++) {
+//   numb = parseInt(prompt("Enter numb:"));
+//   if (numb < 0) {
+//     console.log(`This number is negative: ${numb}`);
+//     break;
+//   }
+// }
+
+//------------------------------------- Unit 2 --------------------------------------------
+
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 3 != 0) {
+//     console.log(i);
+//   }
+// }
+
+//------------------------------------- Unit 3 --------------------------------------------
+
+// for (let i = 2; i <= 12; i++) {
+//   let numb = i ** 3;
+//   console.log(numb);
+// }
+
+//------------------------------------- Unit 4 --------------------------------------------
+
+// const person = {
+//   jonas: 30,
+//   petras: 26,
+//   ona: 45,
+//   maryte: 36,
+//   antanas: 77,
+//   "teta zita": 86,
+// };
+
+// for (let name in person) {
+//   let ages = person[name];
+//   console.log("Name is: " + name, "age is: " + ages);
+// }
+
+//------------------------------------- Unit 5 --------------------------------------------
+
+// const person = {
+//   jonas: 30,
+//   petras: 26,
+//   ona: 45,
+//   maryte: 36,
+//   antanas: 77,
+//   "teta zita": 86,
+// };
+
+// let count = 0;
+// for (let key in person) {
+//   count++;
+// }
+// console.log(count);
+
+//------------------------------------- Unit 6 --------------------------------------------
+
+// let sentence = ["winter", "is", "coming"];
+
+// for (let word of sentence) {
+//   console.log(word);
+// }
+
+//------------------------------------- Unit 7 ????--------------------------------------------
+// let sentence = "Winter is coming";
+// let count = 0;
+// for (let words of sentence) {
+//   count++;
+// }
+// console.log(count);
+// Neisgalvoju iki galo............
+//------------------------------------- Unit 8 --------------------------------------------
+
+// let numb = 20;
+// while (numb <= 50) {
+//   if (numb % 2 == 0) {
+//     console.log(numb);
+//   }
+//   numb++;
+// }
+
+//------------------------------------- Unit 9 --------------------------------------------
+
+// while (true) {
+//   let numb = parseInt(prompt("enter numb: "));
+//   if (numb % 2 != 0) {
+//     alert("Odd number entered ");
+//     break;
+//   }
+// }
+
+//------------------------------------- Unit 10 --------------------------------------------
+// let sum = 0;
+
+// do {
+//   numb = parseInt(prompt("enter numb: "));
+//   if (numb < 0) {
+//     console.log("number is negative");
+//     break;
+//   }
+//   sum += numb;
+// } while (numb != 0);
+// console.log(sum);
